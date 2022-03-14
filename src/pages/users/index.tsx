55066ea3da2eae4127100796e0b862c95fa63421
Deivison Cardoso <deivisondc@gmail.com>
2022-03-14 07:25:10 -0300

    add refetching icon

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -10,7 +10,7 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, isRefetching, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json();
 
@@ -48,7 +48,11 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              
+              {isRefetching && <Spinner size="sm" color="gray.500" ml={4} />}
+            </Heading>
 
             <Link href="/users/create" passHref>
               <Button
