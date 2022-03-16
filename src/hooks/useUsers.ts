9d01602fad7b9bd9ad15d85ebfb642d4a306f7ff
Deivison Cardoso <deivisondc@gmail.com>
2022-03-16 07:28:03 -0300

    fix createdAt format (after add serializer)

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -26,7 +26,7 @@ async function getUsers(page): Promise<GetUsersResponse> {
     id: user.id,
     name: user.name,
     email: user.email,
-    createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+    createdAt: new Date(user.created_at).toLocaleDateString('pt-BR', {
       day: '2-digit',
       month: 'long',
       year: 'numeric',
